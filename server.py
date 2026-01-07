dc931862b022a2a89659c352c9f5df88add7a25d
kamillamamatova <kamillamamatova.km@gmail.com>
2026-01-06 23:14:34 -0500

    Turned Python objects into their serialized counterparts

--- server.py
+++ server.py
@@ -74,9 +74,38 @@ class ProtocolHandler(object):
                     for _ in range(num_items * 2)]
         return dict(zip(elements[::2], elements[1::2]))
 
+    # Serealizes the response data and sends it to the client
     def write_response(self, socket_file, data):
-        # Serealizes the response data and sends it to the client
-        pass
+        buf = BytesIO()
+        self.write(buf, data)
+        buf.seek(0)
+        socket_file.write(buf.getvalue())
+        socket_file.flush()
+
+    def write(self, buf, data):
+        if isinstance(data, str):
+            data = data.encode('utf-8')
+
+        if isinstance(data, bytes):
+            buf.write('$%s\n%s\r\n' % (len(data), data))
+        elif isinstance(data, int):
+            buf.write(':%s\r\n' % data)
+        elif isinstance(data, Error):
+            buf.write('-%s\r\n' % error.message)
+        elif isinstance(data, (list, tuple)):
+            buf.write('*%s\r\n' % len(data))
+            for item in data:
+                self._write(buf, item)
+        elif isinstance(data, dict):
+            buf.write('%%%s\r\n' % len(data))
+            for key in data:
+                self._write(buf, key)
+                self._write(buf, data[key])
+
+        elif data is None:
+            buf.write('$-1\r\n')
+        else:
+            raise CommandError('Unrecognized type: %s' % type(data))
 
 class Server(object):
     # Localhost only
