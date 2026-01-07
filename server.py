f8154077f6f2c223d2d481f9721df88a187ba5ad
kamillamamatova <kamillamamatova.km@gmail.com>
2026-01-06 23:48:07 -0500

    Implemented a client that'll connect to the server and send commands encoded as lists

--- server.py
+++ server.py
@@ -199,3 +199,17 @@ class Server(object):
 
     def run(self):
         self._server.serve_forever()
+
+class Client(object):
+    def __init__(self, host = '127.0.0.1', port = 31337):
+        self._protocol = ProtocolHandler()
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.connect((host, port))
+        self._fh = self._socket.makefile('rwb')
+
+    def execute(self, *args):
+        self._protocol.write_response(self._fh, args)
+        resp = self._protocol.handle_request(self._fh)
+        if isinstance(resp, Error):
+            raise CommandError(resp.message)
+        return resp
