4e455b97f35e4e028173b232daf698611760e8fc
kamillamamatova <kamillamamatova.km@gmail.com>
2026-01-06 23:43:12 -0500

    Implemented the 6 command methods defined in get_commands() method

--- server.py
+++ server.py
@@ -122,6 +122,33 @@ class Server(object):
 
         self._commands = self.get_commands()
 
+    def get(self, key):
+        return self._kv.get(key)
+    
+    def set(self, key, value):
+        self._kv[key] = value
+        return 1
+    
+    def delete(self, key):
+        if key in self._kv:
+            del self._kv[key]
+            return 1
+        return 0
+    
+    def flush(self):
+        kvlen = len(self._kv)
+        self._kv.clear()
+        return kvlen
+    
+    def mget(self, *keys):
+        return [self._kv.get(key) for key in keys]
+    
+    def mset(self, *items):
+        data = zip(items[::2], items[1::2])
+        for key, value in data:
+            self._kv[key] = value
+        return len(data)
+
     def get_commands(self):
         return {
             'GET': self.get,
