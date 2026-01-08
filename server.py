674022490c6843d376ecd73f094aafec300af892
kamillamamatova <kamillamamatova.km@gmail.com>
2026-01-08 00:51:42 -0500

    Made key fixes

--- server.py
+++ server.py
@@ -6,15 +6,12 @@
 # handle errors cleanly,
 # and dicsonnect clients gracefully
 
-# Just a framework so far
-
 from gevent import socket # gevent is a 3rd party python library
 from gevent.pool import Pool # Limits how many clients can be handled at once
 from gevent.server import StreamServer # TCP server that works with gevent
 
 from collections import namedtuple # Lightweight data structure
 from io import BytesIO # Treats bytes like a file
-from socket import error as socket_error # Catches low level socket errors
 import logging
 
 logger = logging.getLogger(__name__)
@@ -44,6 +41,8 @@ class ProtocolHandler(object):
         first_byte = socket_file.read(1)
         if not first_byte:
             raise Disconnect()
+        
+        #first_byte is bytes in Python 3
         first_byte = first_byte.decode()
         
         try:
@@ -55,57 +54,71 @@ class ProtocolHandler(object):
     # For each handler
 
     def handle_simple_string(self, socket_file):
-        return socket_file.readline().rstrip('\r\n')
+        return socket_file.readline().rstrip(b"\r\n").decode("utf-8", errors = "strict")
     
     def handle_error(self, socket_file):
-        return Error(socket_file.readline().rstrip('\r\n'))
+        msg = Error(socket_file.readline().rstrip(b"\r\n").decode("utf-8", errors = "strict"))
+        return Error(msg)
     
     def handle_integer(self, socket_file):
-        return int(socket_file.readline().rstrip('\r\n'))
+        return int(socket_file.readline().rstrip(b"\r\n"))
     
     def handle_string(self, socket_file):
         # First reads the length
-        length = int(socket_file.readline().rstrip('\r\n'))
+        length = int(socket_file.readline().rstrip(b"\r\n"))
+
         # NULL
         if length == -1:
             return None
+        
         length += 2 # Includes the '\r\n' in count
         return socket_file.read(length)[:-2]
     
     def handle_array(self, socket_file):
-        num_items = int(socket_file.readline().rstrip('\r\n'))
-        return [self.handle_request(socket_file) for _ in range(num_items)]
+        num_items = int(socket_file.readline().rstrip(b"\r\n"))
+        elements = [self.handle_request(socket_file) for _ in range(num_items * 2)]
+        return dict(zip(elements[::2], elements[1::2]))
+        #return [self.handle_request(socket_file) for _ in range(num_items)]
 
     # Serealizes the response data and sends it to the client
     def write_response(self, socket_file, data):
         buf = BytesIO()
-        self._write(buf, data)
+        self.write(buf, data)
         buf.seek(0)
         socket_file.write(buf.getvalue())
         socket_file.flush()
 
     def write(self, buf, data):
         if isinstance(data, str):
-            data = data.encode('utf-8')
+            data = data.encode("utf-8")
 
         if isinstance(data, bytes):
-            buf.write(b'$%d\r\n' % len(data))
-            buf.write(data + b'\r\n')
+            buf.write(b"$%d\r\n" % len(data))
+            buf.write(data + b"\r\n")
+
         elif isinstance(data, int):
-            buf.write(':%s\r\n' % data)
+            buf.write(b":%s\r\n" % data)
+
         elif isinstance(data, Error):
-            buf.write('-%s\r\n' % data.message)
+            msg = data.message
+            if isinstance(msg, str):
+                msg = msg.encode("utf-8")
+            buf.write(b"-" + msg + b"\r\n")
+
         elif isinstance(data, (list, tuple)):
-            buf.write('*%s\r\n' % len(data))
+            buf.write(b"*%d\r\n" % len(data))
             for item in data:
                 self._write(buf, item)
+
         elif isinstance(data, dict):
-            buf.write('%%%s\r\n' % len(data))
-            for key in data:
+            buf.write(b"%%%d\r\n" % len(data))
+            for key, value in data.items():
                 self._write(buf, key)
-                self._write(buf, data[key])
+                self._write(buf, value)
+
         elif data is None:
-            buf.write('$-1\r\n')
+            buf.write(b"$-1\r\n")
+
         else:
             raise CommandError('Unrecognized type: %s' % type(data))
 
@@ -121,7 +134,6 @@ class Server(object):
 
         self._protocal = ProtocolHandler()
         self._kv = {}
-
         self._commands = self.get_commands()
 
     def get_commands(self):
@@ -145,7 +157,7 @@ class Server(object):
             try:
                 data = self._protocal.handle_request(socket_file)
             except Disconnect:
-                logger.info('Client disconnected: %s%s' % address)
+                logger.info('Client disconnected: %s:%s' % address[0], address[1])
                 break
 
             try:
@@ -160,22 +172,38 @@ class Server(object):
     # excecutes the command they specified, 
     # and passes back the return value
     def get_response(self, data):
+        # If client sent a simple string, split it into tokens
+        # If client sent an array, it should already be a list
         if not isinstance(data, list):
             try:
+                if isinstance(data, bytes):
+                    data = data.decode("utf-8", errors = "strict")
                 data = data.split()
             except:
-                raise CommandError('Request must be a list or simple string')
+                raise CommandError("Request must be a list or simple string")
         
         if not data:
-            raise CommandError('Missing command')
+            raise CommandError("Missing command")
         
-        command = data[0].upper()
+        cmd = data[0]
+
+        if isinstance(cmd, bytes):
+            cmd = cmd.decode("utf-8", errors = "strict")
+        command = cmd.upper()
+
         if command not in self._commands:
             raise CommandError('Unrecognized command: %s' % command)
-        else:
-            logger.debug('Received %s', command)
         
-        return self._commands[command](*data[1:])
+        logger.debug('Received %s', command)
+
+        args = []
+        for x in data[1:]:
+            if isinstance(x, bytes):
+                args.append(x.decode("utf-8", errors = "strict"))
+            else:
+                args.append(x)
+        
+        return self._commands[command](*args)
 
     def run(self):
         self._server.serve_forever()
@@ -202,10 +230,13 @@ class Server(object):
         return [self._kv.get(key) for key in keys]
     
     def mset(self, *items):
-        data = zip(items[::2], items[1::2])
-        for key, value in data:
+        if len(items) % 2 != 0:
+            raise CommandError("MSET requires an even number of arguments")
+
+        pairs = zip(items[::2], items[1::2])
+        for key, value in pairs:
             self._kv[key] = value
-        return len(data)
+        return len(pairs)
 
 class Client(object):
     def __init__(self, host = '127.0.0.1', port = 31337):
@@ -215,7 +246,7 @@ class Client(object):
         self._fh = self._socket.makefile('rwb')
 
     def execute(self, *args):
-        self._protocol.write_response(self._fh, args)
+        self._protocol.write_response(self._fh, list(args))
         resp = self._protocol.handle_request(self._fh)
         if isinstance(resp, Error):
             raise CommandError(resp.message)
@@ -240,7 +271,11 @@ class Client(object):
         return self.execute('MSET', *items)
     
 if __name__ == '__main__':
-    from gevent import monkey; monkey.patch_all()
+    from gevent import monkey
+    
+    monkey.patch_all()
+
     logger.addHandler(logging.StreamHandler())
     logger.setLevel(logging.DEBUG)
+    
     Server().run()
